62308effe63d8f9f1a5a0b52c417f6cc465ef412
kronodeus <ryangpalmer@hotmail.com>
2025-08-08 16:13:19 -0700

    Removed debug bin

--- bin/nwz.debug.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node --inspect-brk
-import {main} from '../dist/main.js'
-
-main(process.argv.slice(2)).then(() => process.exit(0)).catch(error => {
-    console.error(error instanceof Error ? error.message : error)
-    process.exit(1)
-})
