d1c1591fd78e3d31fdc934184216bfd7731d50ce
kronodeus <ryangpalmer@hotmail.com>
2025-08-09 04:15:12 -0700

    Set up biome and formatted/linted files

--- bin/nwz.js
+++ bin/nwz.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
-import {main} from '../dist/main.js'
+import { main } from '../dist/main.js'
 
-main(process.argv.slice(2)).then(() => process.exit(0)).catch(error => {
-    console.error(error instanceof Error ? error.message : error)
-    process.exit(1)
-})
+main(process.argv.slice(2))
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error instanceof Error ? error.message : error)
+        process.exit(1)
+    })
